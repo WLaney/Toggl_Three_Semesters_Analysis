404375ffe36acc37d4f870737bc751da0c743efb
William Laney <williamlaney@yahoo.com>
2017-04-23 20:57:25 -0400

    clean up comments

--- toggle_analysis.R
+++ toggle_analysis.R
@@ -20,17 +20,27 @@ totals_worked<-function(data,
 	start_date=data$Start.date[1], end_date=data$Start.date[length(data$Start.date)],
 	skips=as.Date(x = integer(0), origin = "1970-01-01"), 
 	proj=data$Project, desc=data$Description){
-		#this function takes a data set with the optional arguments of a start date,
-		#end date, project, desrciption, and skips.
-		#It outputs a plot of the total time spent on the project and 
-		#description combinations per week over the time span.
-		#Weeks start on Monday so this will corrospond to who toggl does weeks
-		#It defaluts to all projects and all descriptions over the time span of the data
+		#this function takes a data set with the optional arguments of:
+		#view by: defalut week, other option day
+		#start date, default first date in data set
+		#end date, default last date in data set
+		#project, default all
+		#desrciption, default all
+		#skips, defalut none
+		#
+		#Outputs a plot of the total time spent for the project and 
+		#description combinations over the time span.
+		#
+		#all dates should be in form "%Y-%m-%d" or "%Y/%m/%d"
+		#
+		#Weeks start on Monday, this corrosponds to how toggl does weeks
+		#
 		#skip gives the ablity to exclude days from the data.
 		#They will not be counted in statistics or ploted
 		#Skips should be a vector that contain the first and last
-		#date included in the average. The dates between these will be exluded
-		#ex: skiped=(start date, end date, start date, end date ...)
+		#date to be included. The dates between these will be exluded
+		#ex input: skiped=c(start date, end date, start date, end date ...)
+		
 		start_date<-as.Date(start_date)
 		end_date<-as.Date(end_date)
 		skips<-as.Date(skips)
@@ -42,20 +52,22 @@ totals_worked<-function(data,
 			date_range_par<-seq(dates[(2*i)-1], dates[2*i], length.out = dates[2*i]-dates[(2*i)-1]+1)
 			date_range<-c(date_range, date_range_par)
 		}
-		
+		#view in terms of weeks
 		if (view_by=="week"){
 			#convert to weeks
 			weeks<-format(date_range, format = "%W")
 			range<-unique(weeks) #find the diffrent weeks we want data for
 			start_date<-format(data$Start.date, format = "%W")
 		}
-		else { #do in terms of day
+		#view in terms of day
+		else {
 			start_date<-data$Start.date
 			range<-date_range
 		}
 		
-		#find the totla duration of each day for the project and description combo used
-		duration<-rep(0, length(range))
+		#find the total duration of each time span 
+		#for the project and description combo used
+		duration<-rep(0, length(range)) #pre allocate memory
 		for(i in 1:length(range)){
 			duration[i]<-sum(data$Duration[start_date==range[i]
 				& data$Project==proj & data$Description==desc])
@@ -63,6 +75,8 @@ totals_worked<-function(data,
 		print(summary(duration))
 		print(sd(duration))
 		
+		#make diffent plots if week or day data
+		#weeks look better with line w/ dot, day looks better with hist
 		if (view_by=="week"){
 			#we need to get the week numbers to dates so that the data ploints in order
 			week_dates<-date_range[match(range, weeks)]
